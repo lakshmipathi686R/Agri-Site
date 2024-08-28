b77d66ff832f91200becea62084b3d3c85d688fe
Your Name <dschannappa93@gmail.com>
2024-08-28 17:04:24 +0530

    created routes

--- Frontend/src/components/About.jsx
+++ Frontend/src/components/About.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      About
-    </div>
+   <>
+   <h1>About</h1>
+   </>
   )
 }
 //dont change the name of function while building
